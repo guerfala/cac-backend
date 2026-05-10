436737e302357160bb664b52567db84afb13a13c
guerfala <mohamedtaher.guerfala@esprit.tn>
2026-05-10 18:05:23 +0200

    refactor : changement de cors origine

--- src/main/java/com/cac/backend/security/SecurityConfig.java
+++ src/main/java/com/cac/backend/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:4201"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
